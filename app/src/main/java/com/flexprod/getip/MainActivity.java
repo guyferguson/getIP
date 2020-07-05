66714282b14ed9d1aea6e24fe602bcd836677c0d
guyferguson <guyferguson@tpg.com.au>
2020-07-05 12:02:16 +1000

    Handles screen rotation, goes to end of scroll, and tells user that Location Services is off, hence no SSID detected - that took me a half day to diagnose, hence the message

--- app/src/main/java/com/flexprod/getip/MainActivity.java
+++ app/src/main/java/com/flexprod/getip/MainActivity.java
@@ -4,17 +4,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.graphics.Color;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.Formatter;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -29,7 +34,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean isGPSEnabled;
     TextView responseView;
     TextView detailsView;
    // ProgressBar progressBar;
@@ -59,9 +64,12 @@ public class MainActivity extends AppCompatActivity {
         s1.append(getExistingFile(filename));
         s1.append(System.getProperty("line.separator"));
 
+        responseView.setMovementMethod(new ScrollingMovementMethod());
         responseView.setText(s1) ;
+    // Scroll to end of TextView
 
         IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        // My Moto G5 Plus cannot go above Android 8.1 (API 27). This action is deprecated at API 28
         filter.addAction(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION);
         this.registerReceiver(mNetworkReceiver, filter);
 
@@ -123,27 +131,51 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+@Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         unregisterNetworkChanges();
     }
 
+
     class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
 
         protected void onPreExecute() {
-            WifiManager wm = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+           WifiManager wm = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
        //     progressBar.setVisibility(View.VISIBLE);
             StringBuilder s1 = new StringBuilder();
             s1.append(responseView.getText());
             s1.append(dtf.format(ZonedDateTime.now(ZoneId.of("Australia/Brisbane"))));
+            if (!(isGPSEnabled)) {
+                s1.append(System.getProperty("line.separator"));
+                s1.append("Location Services disabled - SSID will show as unknown");
+            }
             s1.append(System.getProperty("line.separator"));
             s1.append(wm.getConnectionInfo().getSSID());
             s1.append(System.getProperty("line.separator"));
             responseView.setText(s1);
+            StringBuilder s3 = new StringBuilder();
+
+            if (!(isGPSEnabled)) {
+                s3.append(System.getProperty("line.separator"));
+                s3.append("Location Services disabled - SSID will show as unknown");
+            }
+
             writeFile(filename, getExistingFile(filename)
                     + System.getProperty("line.separator")
                     + dtf.format(ZonedDateTime.now(ZoneId.of("Australia/Brisbane")))
+                    + s3
                     + System.getProperty("line.separator")
                     + wm.getConnectionInfo().getSSID());
             //Disable the button to avoid multiple calls overrunning each other
@@ -154,7 +186,6 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(Void... urls) {
             // Do some validation here
             Log.d("GetIP", "About to call to amazon");
-
             try {
                 URL url = new URL(API_URL);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -196,9 +227,8 @@ public class MainActivity extends AppCompatActivity {
             //Re-enable the Get IP button
             Button queryButton = findViewById(R.id.button);
             queryButton.setEnabled(true);
+
         }
-
-
     }
 
      class MyReceiver extends BroadcastReceiver
@@ -208,6 +238,14 @@ public class MainActivity extends AppCompatActivity {
         {
             try
             {
+                // Check if location servcies are available
+                LocationManager locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    isGPSEnabled = true;
+                } else {
+                   responseView.setText(System.getProperty("line.separator"));
+                   responseView.setText("Location Services disabled - SSID will show as unknown");
+                }
                 String id = intent.getStringExtra("notificationID");
                 HashMap<String, String> myMap = new HashMap<String, String>();
                 myMap.put("1","test");
@@ -220,6 +258,10 @@ public class MainActivity extends AppCompatActivity {
                 responseView.setBackgroundColor(Color.GREEN);
                 responseView.setTextColor(Color.WHITE);
                 s1.setLength(0);
+                WifiManager wm = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wm.getConnectionInfo();
+                String ssid = wifiInfo.getSSID();
+                s1.append(wm.getConnectionInfo().getSSID());
                 s1.append(responseView.getText());
                 responseView.setTextColor(Color.RED);
                 s1.append("Network change detected");
@@ -241,9 +283,6 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-
-
     }
-
 }
 
